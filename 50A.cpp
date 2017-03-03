68bcdf0fd7c464daa44f7118a34ae6bf019bc7c8
Ebey Abraham <ebey97@gmail.com>
2017-03-03 18:24:57 +0530

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,16 @@
+/*
+* Link to the question:
+* http://codeforces.com/problemset/problem/50/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int INT;
+
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	int ans=m*n/2;
+	cout<<ans;
+	return 0;
+}

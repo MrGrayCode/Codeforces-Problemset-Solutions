a13e1741b807d4d4acb96bff6785d079ea31d850
Ebey Abraham <ebey97@gmail.com>
2017-03-03 14:18:26 +0530

    Create 158A.cpp

---
+++ 158A.cpp
@@ -0,0 +1,28 @@
+/*
+* Link for the problem statement
+* http://codeforces.com/problemset/problem/158/A
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int INT;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		int n=s.length();
+		if(n<=10)
+		{
+			cout<<s<<"\n";
+		}
+		else
+		{
+			cout<<s[0]<<n-2<<s[n-1]<<"\n";
+		}
+	}
+	return 0;
+}

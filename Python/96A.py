7880bafc03f7ee9530ab585bd7f68d9bafbc587d
Ebey Abraham <ebey97@gmail.com>
2017-03-03 21:34:55 +0530

    Create 96A.py

---
+++ Python/96A.py
@@ -0,0 +1,11 @@
+'''
+* Link for the problem statement
+* http://codeforces.com/problemset/problem/96/A
+'''
+s=input().strip()
+case1='1111111'
+case2='0000000'
+if case1 in s or case2 in s:
+    print('YES')
+else:
+    print('NO')

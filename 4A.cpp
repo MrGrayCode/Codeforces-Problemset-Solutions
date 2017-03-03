df8face6a07bcdcbba72d28e09f49c696a3a2e8e
Ebey Abraham <ebey97@gmail.com>
2017-03-03 13:58:29 +0530

    Create 4A.cpp

---
+++ 4A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int INT;
+int main()
+{
+	int w;
+	cin>>w;
+	if(w<=2 || w%2)
+	{
+		cout<<"NO";
+	}
+	else
+	{
+		cout<<"YES";
+	}
+	return 0;
+}

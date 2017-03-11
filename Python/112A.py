849d2b48dcc0cb47305212a68ec99a894d8d9deb
Ebey Abraham <ebey97@gmail.com>
2017-03-11 17:25:40 +0530

    Create 112A.py

---
+++ Python/112A.py
@@ -0,0 +1,14 @@
+'''
+* Link for the problem statement
+* http://codeforces.com/problemset/problem/112/A
+'''
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+if s1==s2:
+    print('0')
+elif s1<s2:
+    print('-1')
+else:
+    print(1)
